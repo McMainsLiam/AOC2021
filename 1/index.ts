458aa62be8c39f6af3a1964bd26079d96d794bc4
Liam McMains <liam@mcmains.net>
2021-12-01 14:14:23 -0600

    Consistency

--- 1/index.ts
+++ 1/index.ts
@@ -5,7 +5,7 @@ const numbers = convertInputToNumbersList(text);
 
 // Part 1
 const result = numbers.reduce((previous, _, index, array) => {
-  return previous + (array[index] < (array[index + 1] || -Infinity) ? 1 : 0);
+  return previous + (array[index] < (array[index + 1] ?? 0) ? 1 : 0);
 }, 0);
 
 console.log(result);
@@ -15,7 +15,7 @@ const secondResult = numbers.reduce((previous, _, index, array) => {
   const isGreater =
     array[index] + (array[index + 1] ?? 0) + (array[index + 2] ?? 0) <
     (array[index + 1] ?? 0) + (array[index + 2] ?? 0) + (array[index + 3] ?? 0);
-    
+
   return previous + (isGreater ? 1 : 0);
 }, 0);
 
