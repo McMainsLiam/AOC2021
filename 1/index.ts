62b76602618cf1f79e8ec5918b2f0891ac1dbb62
Liam McMains <liam@mcmains.net>
2021-12-01 14:12:45 -0600

    Add day one challenge

--- 1/index.ts
+++ 1/index.ts
@@ -1,3 +1,22 @@
 const text = await Deno.readTextFile("./input");
+const convertInputToNumbersList = (text: string) =>
+  text.split("\n").map(Number);
+const numbers = convertInputToNumbersList(text);
 
-console.log(text);
+// Part 1
+const result = numbers.reduce((previous, _, index, array) => {
+  return previous + (array[index] < (array[index + 1] || -Infinity) ? 1 : 0);
+}, 0);
+
+console.log(result);
+
+// Part 2
+const secondResult = numbers.reduce((previous, _, index, array) => {
+  const isGreater =
+    array[index] + (array[index + 1] ?? 0) + (array[index + 2] ?? 0) <
+    (array[index + 1] ?? 0) + (array[index + 2] ?? 0) + (array[index + 3] ?? 0);
+    
+  return previous + (isGreater ? 1 : 0);
+}, 0);
+
+console.log(secondResult);
