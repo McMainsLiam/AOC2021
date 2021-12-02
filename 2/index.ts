4392e738df2aa27333e27daec18d9523f4c0b7e3
Liam McMains <liam@mcmains.net>
2021-12-01 23:30:30 -0600

    Day 2 complete, Day 3 setup

--- 2/index.ts
+++ 2/index.ts
@@ -1,5 +1,40 @@
 const text = await Deno.readTextFile("2/input");
 
-console.log("Day 2!");
+const lines = text.split("\n");
+
+const entries = lines
+  .map((line) => line.split(" "))
+  .map(([direction, distance]) => [direction, Number(distance)]) as [
+  string,
+  number
+][];
+
+// Part 1
+let totalDistance = 0,
+  totalDepth = 0;
+
+entries.forEach(([direction, distance]) => {
+  if (direction === "up" || direction === "down")
+    totalDepth += direction === "down" ? distance : -distance;
+  else totalDistance += distance;
+});
+
+console.log(totalDistance * totalDepth);
+
+// Part 2
+let aim = 0;
+totalDistance = 0;
+totalDepth = 0;
+
+for (const [direction, x] of entries) {
+  if (direction === "up" || direction === "down") {
+    aim += (direction === "down" ? 1 : -1) * x;
+  } else {
+    totalDistance += x;
+    totalDepth += aim * x;
+  }
+}
+
+console.log(totalDistance * totalDepth);
 
 export {};
---
+++ 3/index.ts
@@ -0,0 +1,5 @@
+const text = await Deno.readTextFile("3/input");
+
+console.log("Day 3!");
+
+export {};
